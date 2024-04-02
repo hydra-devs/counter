55b62080fbd5ac6eb7c1b9b3d1f893d5a28f8519
Hydra Dev <52891699+hydra-devs@users.noreply.github.com>
2024-04-02 16:44:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const label = document.getElementById("counterLabel");
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
+const reset = document.getElementById("reset");
+
+let count = 0;
+
+increase.onclick = function () {
+  count++;
+  label.textContent = count;
+};
+
+decrease.onclick = function () {
+  count--;
+  label.textContent = count;
+};
+
+reset.onclick = function () {
+  count = 0;
+  label.textContent = count;
+};
